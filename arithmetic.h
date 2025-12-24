6a490d26dccf57b85a6b391a01a7e8673a230375
schloing <user.datagram.protocol@proton.me>
2025-12-24 16:11:10 +1100

    add fundamental arithmetic nodes

--- arithmetic.h
+++ arithmetic.h
@@ -11,6 +11,41 @@ public:
         : IRNode({a, b}), a(a), b(a) {}
 };
 
+// FIXME: too repetitive, bring common functionality into a wrapper in ArithmeticBinaryExpr or sum shi
+class Add : public ArithmeticBinaryExpr {
+public:
+    using ArithmeticBinaryExpr::ArithmeticBinaryExpr;
+
+    Type* compute() override {
+        if (auto ai = dyn_cast<TypeInteger>(a->type);
+            auto bi = dyn_cast<TypeInteger>(b->type)) {
+            // TODO: add alternative
+            if (a->type->is_const() && b->type->is_const()) {
+                return new TypeInteger(ai->value + bi->value);
+            }
+        }
+
+        return new Type();
+    }
+};
+
+class Subtract : public ArithmeticBinaryExpr {
+public:
+    using ArithmeticBinaryExpr::ArithmeticBinaryExpr;
+
+    Type* compute() override {
+        if (auto ai = dyn_cast<TypeInteger>(a->type);
+            auto bi = dyn_cast<TypeInteger>(b->type)) {
+            // TODO: add alternative
+            if (a->type->is_const() && b->type->is_const()) {
+                return new TypeInteger(ai->value - bi->value);
+            }
+        }
+
+        return new Type();
+    }
+};
+
 class Multiply : public ArithmeticBinaryExpr {
 public:
     using ArithmeticBinaryExpr::ArithmeticBinaryExpr;
@@ -24,7 +59,42 @@ public:
             }
         }
 
-        return new Type(Type::Kind::BOT);
+        return new Type();
+    }
+};
+
+class Divide : public ArithmeticBinaryExpr {
+public:
+    using ArithmeticBinaryExpr::ArithmeticBinaryExpr;
+
+    Type* compute() override {
+        if (auto ai = dyn_cast<TypeInteger>(a->type);
+            auto bi = dyn_cast<TypeInteger>(b->type)) {
+            // TODO: add alternative
+            if (a->type->is_const() && b->type->is_const()) {
+                return new TypeInteger(ai->value / bi->value);
+            }
+        }
+
+        return new Type();
+    }
+};
+
+class Negate : public IRNode {
+public:
+    IRNode* a;
+
+    Negate(IRNode* a) : IRNode({a}) {}
+
+    Type* compute() override {
+        if (auto ai = dyn_cast<TypeInteger>(a->type)) {
+            // TODO: add alternative
+            if (a->type->is_const()) {
+                return new TypeInteger(-ai->value);
+            }
+        }
+
+        return new Type();
     }
 };
 } // namespace despair
